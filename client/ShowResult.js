1f775514e803866c864664fb112dc4d2c157eeb1
Anatoly Gusev <avg.tolik@gmail.com>
2016-04-22 10:02:29 +0300

    Update render algorithm of ShowResult

--- client/ShowResult.js
+++ client/ShowResult.js
@@ -8,25 +8,25 @@ const ShowResult = (props) => {
   if (props.repo === null) {
     return null;
   }
+  const data = [
+    { label: 'Full Name:', value: props.repo.full_name },
+    { label: 'Description:', value: props.repo.description },
+    { label: 'Language:', value: props.repo.language },
+    { label: 'Link:', value: formatLink(props.repo.url) },
+  ];
+  
+  const rows = data.map((value) => (
+    <tr key={Math.random()}>
+      <td className="label right aligned">{value.label}</td>
+      <td>{value.value}</td>
+    </tr>
+  ));
   
   return (
     <table className="result-table ui celled table">
-      <tr>
-        <td className="label right aligned">Full Name:</td>
-        <td>{props.repo.full_name}</td>
-      </tr>
-      <tr>
-        <td className="label right aligned">Description:</td>
-        <td>{props.repo.description}</td>
-      </tr>
-      <tr>
-        <td className="label right aligned">Language:</td>
-        <td>{props.repo.language}</td>
-      </tr>
-      <tr>
-        <td className="label right aligned">Link:</td>
-        <td>{formatLink(props.repo.url)}</td>
-      </tr>
+      <tbody>
+        {rows}
+      </tbody>
     </table>
   );
 };
